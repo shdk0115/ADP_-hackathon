79c3e980865baa8f9fe40a1ed959a386f3afe8b1
paulpark <sansungpark@mz.co.kr>
2026-04-28 11:02:44 +0900

    Update default_search.py

--- opensearch/default_search.py
+++ opensearch/default_search.py
@@ -1,1 +1,30 @@
+def default_hybrid_search(client, index_name, query, query_vector, size=5):
+    body = {
+        "size": size,
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "match": {
+                            "contents": {
+                                "query": query,
+                                "boost": 0.5
+                            }
+                        }
+                    },
+                    {
+                        "knn": {
+                            "embedding": {
+                                "vector": query_vector,
+                                "k": size,
+                                "boost": 0.5
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
 
+    response = client.search(index=index_name, body=body)
+    return response["hits"]["hits"]
