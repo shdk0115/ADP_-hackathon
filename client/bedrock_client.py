ff6710bf3dd1a779f9cd9dd4a6e12aacd04344b7
paulpark <sansungpark@mz.co.kr>
2026-04-28 11:32:27 +0900

    Create bedrock_client.py

---
+++ client/bedrock_client.py
@@ -0,0 +1,17 @@
+from langchain.llms import Bedrock
+from langchain.embeddings import BedrockEmbeddings
+from config.config import AWS_REGION, BEDROCK_TEXT_MODEL, BEDROCK_EMBED_MODEL
+
+
+def get_text_llm():
+    return Bedrock(
+        region_name=AWS_REGION,
+        model_id=BEDROCK_TEXT_MODEL
+    )
+
+
+def get_embedding_model():
+    return BedrockEmbeddings(
+        region_name=AWS_REGION,
+        model_id=BEDROCK_EMBED_MODEL
+    )
